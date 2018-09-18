5011ba2a80a1f469a6fd5c1157a8790d90f3c290
TijsT <42801544+TijsT@users.noreply.github.com>
2018-09-18 10:20:42 +0200

    repository cleanup

---
+++ javascript101/assets/js/app.js
@@ -0,0 +1,11 @@
+function isOdd(num) { return num % 2;}
+console.log("8 is " + isOdd(8));
+console.log("1 is odd 0 is even");
+
+const str ="Programming is not cool"
+console.log(str)
+const newStr = str.replace(" not", "")// " not" word vervangen met niks
+console.log(newStr)
+
+/*Deze vergelijking is altijd false 1400 komt nooit overeen met een zin*/
+console.log(1400=="Ik woon in Naboo")
